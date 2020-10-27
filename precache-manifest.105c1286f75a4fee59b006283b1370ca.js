ef18bfec92210dd70f47526ef5039232f93c50a4
zhaodan <450535034@qq.com>
2020-10-27 10:15:08 +0800

    deploy

---
+++ precache-manifest.105c1286f75a4fee59b006283b1370ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d9b75e591246175ba9",
+    "url": "/fish-money-page/css/app.ebbe718b.css"
+  },
+  {
+    "revision": "d43ac612fae07a669ca136dd55741c80",
+    "url": "/fish-money-page/index.html"
+  },
+  {
+    "revision": "b3d9b75e591246175ba9",
+    "url": "/fish-money-page/js/app.3615cc3d.js"
+  },
+  {
+    "revision": "3c7c4e83b5b60350e97c",
+    "url": "/fish-money-page/js/chunk-vendors.bfba69d8.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/fish-money-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fish-money-page/robots.txt"
+  }
+]);
